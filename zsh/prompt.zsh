c87d710b59a09190fd6fb61c0580785545d59c91
Jonathan Clem <jonathan@jclem.net>
2014-07-11 09:43:29 -0400

    zsh: add basic zshrc and prompt

---
+++ zsh/prompt.zsh
@@ -0,0 +1,34 @@
+autoload colors && colors
+setopt prompt_subst
+
+git_branch () {
+  ref=$(/usr/bin/git symbolic-ref HEAD 2> /dev/null | awk -F/ {'print $NF'})
+
+  if [[ $ref == "" ]]; then
+    echo ""
+  else
+    echo " %{$fg[cyan]%}{ %{$fg[red]%}$ref%{$reset_color%}%{$fg[cyan]%} }%{$reset_color%}"
+  fi
+}
+
+git_dirty () {
+  is_git=$([[ -d .git ]] && echo "true")
+
+  if [[ $is_git == "true" ]]; then
+    st=$(/usr/bin/git status 2> /dev/null | tail -n 1)
+
+    if [[ $st == "nothing to commit, working directory clean" ]]; then
+      echo %{$fg[green]%} ✓%{$reset_color%}
+    else
+      echo %{$fg[red]%} ✗%{$reset_color%}
+    fi
+  else
+    echo ""
+  fi
+}
+
+export PROMPT='%{$fg_bold[red]%}➼\
+  %{$reset_color%}%{$fg[blue]%}%c%{$reset_color%}\
+ %{$fg[yellow]%}[%m% ]%{$reset_color%}\
+$(git_branch)$(git_dirty) '
+
