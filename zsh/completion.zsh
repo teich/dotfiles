d7a8ab28b72998afe5864665bd47164b5c6398b6
Jonathan Clem <jonathan@jclem.net>
2014-07-22 18:15:26 -0400

    zsh: basic completion, including hk

---
+++ zsh/completion.zsh
@@ -0,0 +1,3 @@
+zstyle ':completion:*' menu select=2                # menu complete w/2+ results
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # smart-casing
+zstyle ':completion:*' list-colors ''               # show colors in list
