251329cc4f1288083d6b6e03abae21e8b06c8292
Oren Teich <oren@teich.net>
2016-04-28 10:03:29 -0700

    remove zsh, use bash

--- zsh/completion.zsh
+++
@@ -1,3 +0,0 @@
-zstyle ':completion:*' menu select=2                # menu complete w/2+ results
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' # smart-casing
-zstyle ':completion:*' list-colors ''               # show colors in list
